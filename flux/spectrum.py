fca44a6bf186f061b90759e664cd9d9af9eaf030
John Boyington <mjcb@ksu.edu>
2019-01-24 10:55:52 -0600

    added plotting function

--- flux/spectrum.py
+++ flux/spectrum.py
@@ -48,3 +48,64 @@ class Spectrum(object):
             edges = np.concatenate(np.array([floor]), np.array(edges))
         elif len(edges) == len(values) + 1:
             edges = np.array(edges)
+
+        # calculate midpoints and bin edges
+        self.widths = edges[1:] - edges[:-1]
+        self.midpoints = (edges[1:] + edges[:-1]) / 2
+
+        # store other primary values
+        self.edges = edges
+
+        # store values and error in differential and integral forms
+        if form in ('int', 'integral'):
+            self.int = values
+            self.int_error = error
+            self.diff = self.int / self.widths
+            self.diff_error = self.int_error / self.widths
+
+        elif form in ('dif', 'diff', 'differential'):
+            self.diff = values
+            self.diff_error = error
+            self.int = self.diff * self.widths
+            self.int_error = self.int_error * self.widths
+
+        return
+
+    def plot(self, plot_type, form):
+        """This function will return the arguments necessary for plotting
+        the data with both plt.plot and plt.errorbar in both integral and
+        differential form."""
+
+        # check inputs
+        message = "Plot type must be literal of either plot or errorbar."
+        assert plot_type in ('plot', 'errorbar'), message
+        message = "Spectrum form option must be literal in ('int', 'integral', 'dif', 'diff', 'differential')."
+        assert form in ('int', 'integral', 'dif', 'diff', 'differential'), message
+
+        # step looking data on a plt.plot
+        if plot_type == 'plot':
+
+            # create doubles of bin edges
+            X = np.array([[xx, xx] for xx in np.array(self.edges)]).flatten()[1:-1]
+
+            # plotting the integral form
+            if form in ('int', 'integral'):
+                Y = np.array([[yy, yy] for yy in np.array(self.int)]).flatten()
+
+            # plotting the differential form
+            elif form in ('dif', 'diff', 'differential'):
+                Y = np.array([[yy, yy] for yy in np.array(self.diff)]).flatten()
+
+            # return the x and y points
+            return X, Y
+
+        # for errorbars in the appropriate locations
+        elif plot_type == 'errorbar':
+
+            # errorbars on the integral form
+            if form in ('int', 'integral'):
+                return self.midpoints, self.int, self.int_error
+
+            # errorbars on the differential form
+            elif form in ('dif', 'diff', 'differential'):
+                return self.midpoints, self.diff, self.diff_error
