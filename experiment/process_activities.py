11f74b9b87f650f0ecabdb2b78e3e9d7b0dcd258
John Boyington <mjcb@ksu.edu>
2019-04-08 13:07:41 -0500

    pulled activities from foil RPT files

--- experiment/process_activities.py
+++ experiment/process_activities.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 import matplotlib.pyplot as plt
 
 
@@ -8,10 +9,27 @@ def extract_foil(filename):
     with open(filename, 'r') as F:
         output = F.read()
 
-    print(output)
+    # grab all tally data
+    pattern = re.compile(r'AU-198     \d.\d\d\d      \d.\d\d\d\d\d\dE[+-]\d\d\d   \d.\d\d\d\d\d\dE[+-]\d\d\d')
+    results = re.findall(pattern, output)
 
-    return
+    # convert to np array
+    return np.array([float(r) for r in results[0].split()[2:]])
 
 
-def sample_foil():
-    """Extract the data from a """
+def au_activities():
+    """Extract the data from the gold foil measurements"""
+
+    foil_ids = ('2', '13', '4', '5', '6')
+    foil_activities = np.zeros((len(foil_ids), 2))
+
+    for i, foil_id in enumerate(foil_ids):
+
+        foil_activities[i] = extract_foil('4_5_19/au' + foil_id + '.RPT')
+
+    return foil_activities
+
+
+if __name__ == '__main__':
+    fa = au_activities()
+    print(fa)
